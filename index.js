55af43fbe1e90f1f62abe4b02b144a8ebcbd9684
skk7414 <skk7414@naver.com>
2021-02-25 01:40:15 +0900

    bot upload

--- index.js
+++ index.js
@@ -260,5 +260,47 @@ client.on('message', async message => {
     message.channel.send(`${Mention_member}, 해방됨`)
   }
 })
+//아바타 확인
 
-client.login(token)
+module.exports = {
+  name: 'avatar',
+  description: 'Get the avatar URL of the tagged user(s), or your own avatar.',
+  execute(message) {
+      if (!message.mentions.users.size) {
+          return message.channel.send(`Your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
+      }
+
+      const avatarList = message.mentions.users.map(user => {
+          return `${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`;
+      });
+
+      message.channel.send(avatarList);
+  },
+};
+
+if (message.content === '!코로나') {
+  let url = "https://apiv2.corona-live.com/stats.json"
+  request(url, (error, response, body) => {
+      let overview = JSON.parse(response.body).overview;
+      overview = {
+          total_confirmed_person: overview.confirmed[0], // 총 확진자수
+          yesterday_confirmed_person: overview.confirmed[1], // 어제 확진자수
+  
+          current_confirmed_person: overview.current[0], // 현재 확진자수
+          current_confirmed_person_diff: overview.current[1], // diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+      }
+  
+      let embed = new Discord.MessageEmbed()
+      embed.setTitle('코로나 라이브 홈페이지')
+      embed.setURL('https://corona-live.com')
+      embed.setColor('#FF8000')
+      embed.setDescription('코로나 정보입니다')
+      embed.addField(`대한민국 총 확진자 수`, `${overview.total_confirmed_person}명`, true)
+      embed.addField(`어제 확진자 수`, overview.yesterday_confirmed_person + `명`, true)
+      embed.addField(`오늘 확진자 수`, overview.current_confirmed_person + `명`, true)
+      // embed.addField(`오늘 어제지금시간   -   현재지금시간의 확진자`, overview.current_confirmed_person_diff + `명`, true)
+      message.channel.send(embed)
+  
+    })
+}
+client.login(token);
